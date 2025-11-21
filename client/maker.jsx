dc6025ef025a1b5ff107b474afa19f3865d9eeab
kaigidwani <kpg8244@rit.edu>
2025-11-21 14:59:34 -0500

    Created start of Selection method to give list options event handlers for selection

--- client/maker.jsx
+++ client/maker.jsx
@@ -14,11 +14,19 @@ const handleCharacter = (e, onCharacterAdded) => {
     helper.hideError();
 
     const name = e.target.querySelector('#characterName').value;
-    const faceList = e.target.querySelector('#characterFace').querySelector("#selected");
+    
+    const faceList = e.target.querySelector('#characterFace')
     const skinColorList = e.target.querySelector('#characterSkinColor').querySelector("#selected");
     const hairList = e.target.querySelector('#characterHair').querySelector("#selected");
     const topList = e.target.querySelector('#characterTop').querySelector("#selected");
     const bottomsList = e.target.querySelector('#characterBottoms').querySelector("#selected");
+    
+    const selectedFace = faceList.querySelector("#selected");
+    const selectedSkinColor = skinColorList.querySelector("#selected");
+    const selectedhHair = hairList.querySelector("#selected");
+    const selectedTop = topList.querySelector("#selected");
+    const selectedBottoms = bottomsList.querySelector("#selected");
+
     // best way to copy what picrew does is have a ul
     // and check which of the li has the class "selected"
 
@@ -28,19 +36,19 @@ const handleCharacter = (e, onCharacterAdded) => {
     }
 
     // Get selected items from each list
-    const face = faceList.getAttribute("data");
+    const face = selectedFace.getAttribute("data");
     console.log("Face: " + face);
 
-    const skinColor = skinColorList.getAttribute("data");
+    const skinColor = selectedSkinColor.getAttribute("data");
     console.log("Skin color: " + skinColor);
 
-    const hair = hairList.getAttribute("data");
+    const hair = selectedhHair.getAttribute("data");
     console.log("Hair: " + hair);
 
-    const top = topList.getAttribute("data");
+    const top = selectedTop.getAttribute("data");
     console.log("Top: " + top);
 
-    const bottoms = bottomsList.getAttribute("data");
+    const bottoms = selectedBottoms.getAttribute("data");
     console.log("Bottoms: " + bottoms);
 
     helper.sendPost(e.target.action, {name, face, skinColor, hair, top, bottoms}, onCharacterAdded);
@@ -157,6 +165,51 @@ const App = () => {
         </div>
     );
 };
+
+const Selection = () => {
+    // Get the options
+    const faceOptions = {
+        domoface: "assets/img/characterOptions/fc_domoface.jpeg",
+        face: "assets/img/characterOptions/fc_face.png",
+    };
+
+    // Grab the above from props given in
+    // props.faceOptions
+    // Then in App (above) call it with CharacterForm and with props defined there
+    // Create function to give back hard coded file paths for options (like faceOptions)
+    // determine which is given by a string, use that string in props for this func too
+    // in order to fill out the ul id and name
+
+    let faceOptionsHTML = "";
+
+    // Create html for options
+    for (let i = 0; i < faceOptions.length; i++) {
+        faceOptionsHTML +=
+            <li id="" data={faceOptions[i]}>
+                <img src={faceOptions[i]}/>
+            </li>;
+    }
+
+    for (let i = 0; i < faceOptions.length; i++) {
+        // Add event listener for click and add class, and remove others
+        faceOptions[i].addEventListener("click", () => {
+            // Remove selected from all other options
+            for (let j = 0; j < faceOptions.length; j++) {
+                faceOptions[j].classList.remove("selected");
+            };
+
+            // Add the selected class
+            faceOptions[i].classList.add("selected");
+        });
+    }
+
+    let faceHTML =
+        <ul id="characterFace" name="face">Face:
+            {faceOptionsHTML}
+        </ul>;
+
+    return faceHTML;
+}
 
 const init = () => {
     const root = createRoot(document.getElementById('app'));
